b1bc487acbe9e9c1c902052b4d9baac0f72fae31
Konstantin Popopvskikh <k.popovskih@vsmrk.ru>
2022-03-19 14:23:14 +0300

    cleaned up arrays

--- lib/utils/pizza_stock.dart
+++ lib/utils/pizza_stock.dart
@@ -1,22 +1,8 @@
 import 'package:sgmk_flutter_pizza/models/pizza.dart';
-import 'package:sgmk_flutter_pizza/utils/ui.dart';
-import 'package:uuid/uuid.dart';
 
-List<Pizza> stock = [
-  Pizza(
-    id: const Uuid().v4(),
-    name: 'Original',
-    assetPicture: PizzaAssets.originalPizzaPicture,
-    quantity: 2,
-    price: 8,
-  ),
-];
+List<Pizza> stock = [];
 
-List<Pizza> marketItems = [
-  ...stock.map(
-    (pizza) => pizza.copyWith(quantity: 0),
-  ),
-];
+List<Pizza> marketItems = [];
 
 List<Pizza> myOrders = [];
 
